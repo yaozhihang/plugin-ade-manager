9a52e8a8eb45cd636c9fc7e28c7ef4c329389ff5
Zhihng Yao <zhihang.yao@tum.de>
2017-12-01 16:52:19 +0100

    backup

--- src/org/citydb/plugins/ade_manager/cleanup/CitydbCleanupScriptGenerator.java
+++ src/org/citydb/plugins/ade_manager/cleanup/CitydbCleanupScriptGenerator.java
@@ -3,17 +3,20 @@ package org.citydb.plugins.ade_manager.cleanup;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ddlutils.Platform;
 import org.apache.ddlutils.PlatformInfo;
-import org.apache.ddlutils.platform.SqlBuilder;
+import org.citydb.database.schema.mapping.AbstractJoin;
 import org.citydb.database.schema.mapping.AbstractProperty;
 import org.citydb.database.schema.mapping.FeatureProperty;
 import org.citydb.database.schema.mapping.FeatureType;
+import org.citydb.database.schema.mapping.JoinTable;
 import org.citydb.database.schema.mapping.SchemaMapping;
 
 public class CitydbCleanupScriptGenerator {
+	private final String space = " ";
 	private File outputFile;
 	
 	public CitydbCleanupScriptGenerator(File outputFile) {
@@ -28,20 +31,33 @@ public class CitydbCleanupScriptGenerator {
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(outputFile);
-			SqlBuilder sqlBuilder = new SqlBuilder(databasePlatform) {};		
-			sqlBuilder.setWriter(writer);
-			
+
 			List<FeatureType> featureTypes = mainSchemaMapping.getFeatureTypes();
+			List<BinaryJoinEntry> entryList = new ArrayList<BinaryJoinEntry>();
+			
 			for (FeatureType featureType : featureTypes) {
 				for (AbstractProperty property : featureType.getProperties()) {
 					if (property instanceof FeatureProperty) {
-						FeatureType targetFeatureType = ((FeatureProperty) property).getType();
+						FeatureProperty featureProperty = ((FeatureProperty) property);
+						FeatureType targetFeatureType = featureProperty.getType();
 						if (targetFeatureType.getId().equalsIgnoreCase( "AddressType")) {
-							printComment(featureType.getId() + "-->" + targetFeatureType.getId(), databasePlatform, writer);							
+							AbstractJoin join = featureProperty.getJoin();
+							if (join instanceof JoinTable) {
+								String joinTable = ((JoinTable) join).getTable();
+								String targetTable = ((JoinTable) join).getInverseJoin().getTable();
+								String sourceJoinColumn = ((JoinTable) join).getJoin().getFromColumn();
+								String targetJoinColumn = ((JoinTable) join).getInverseJoin().getFromColumn();
+								BinaryJoinEntry entry = new BinaryJoinEntry(joinTable, targetTable, sourceJoinColumn, targetJoinColumn);
+								
+								entryList.add(entry);
+								
+							//	printComment(entry.getSourceTable() + "-->" + entry.getJoinTable() + "<--" + entry.getTargetTable(), databasePlatform, writer);	
+							}													
 						}
 					}
 				}					
 			}
+			buildSql(entryList);
 		} catch (IOException e) {
 			throw new CcgException("Failed to open file '" + outputFile.getName() + "' for writing.", e);
 		} finally {
@@ -57,6 +73,49 @@ public class CitydbCleanupScriptGenerator {
 		this.outputFile = outputFile;
 	}
 
+	/*
+	 * 'SELECT ad.id FROM %I.address ad
+       LEFT OUTER JOIN %I.address_to_building ad2b ON ad2b.address_id = ad.id
+       LEFT OUTER JOIN %I.address_to_bridge ad2brd ON ad2brd.address_id = ad.id
+       LEFT OUTER JOIN %I.opening o ON o.address_id = ad.id
+       LEFT OUTER JOIN %I.bridge_opening brdo ON brdo.address_id = ad.id
+       WHERE ad2b.building_id IS NULL
+         AND ad2brd.bridge_id IS NULL
+         AND o.address_id IS NULL
+         AND brdo.address_id IS NULL',
+	 */
+	private void buildSql(List<BinaryJoinEntry> entryList) {		
+		String aliasSourceTablePrefix = "s";
+		String aliasJoinTablePrefix = "j";
+		String aliasTargetTable = "t";
+		String targetTable = entryList.get(0).getTargetTable();
+		
+		StringBuilder joinBuilder = new StringBuilder();		
+		joinBuilder.append("select").append(space).append(aliasTargetTable).append(".id FROM %I.")
+		.append(targetTable).append(space).append(aliasTargetTable).append(space).append(System.lineSeparator());;
+		
+		StringBuilder clauseBuilder = new StringBuilder();
+		clauseBuilder.append("WHERE").append(space);
+		
+		int index = 1;
+		for (BinaryJoinEntry entry: entryList) {
+			String aliasJoinTable = aliasJoinTablePrefix + index;
+			joinBuilder.append("LEFT OUTER JOIN %I.").append(entry.getJoinTable()).append(space).append(aliasJoinTable).append(space).append("ON")
+			.append(space).append(aliasJoinTable).append(".").append(entry.getTargetJoinColumn()).append(space).append("=").append(space)
+			.append(aliasSourceTablePrefix).append(".id").append(space).append(System.lineSeparator());
+			
+			if (index > 1)
+				clauseBuilder.append("AND").append(space);				
+			clauseBuilder.append(aliasJoinTable).append(".").append(entry.getSourceJoinColumn()).append(space).append("IS NULL").append(space).append(System.lineSeparator());;
+			
+			index++;			
+		}
+		
+		joinBuilder.append(clauseBuilder.toString());
+		
+		System.out.println(joinBuilder.toString());
+	}
+
 	private void printComment(String text, Platform platform, PrintWriter writer) throws IOException
     {
         if (platform.isSqlCommentsOn())
@@ -70,4 +129,35 @@ public class CitydbCleanupScriptGenerator {
         	writer.println();
         }
     }
+	
+	private class BinaryJoinEntry {
+		private String joinTable;
+		private String targetTable;
+		private	String sourceJoinColumn;
+		private String targetJoinColumn;
+		
+		protected BinaryJoinEntry(String joinTable, String targetTable, String sourceJoinColumn, String targetJoinColumn){
+			this.joinTable = joinTable;
+			this.targetTable = targetTable;
+			this.sourceJoinColumn = sourceJoinColumn;
+			this.targetJoinColumn = targetJoinColumn;
+		}
+		
+		protected String getJoinTable() {
+			return joinTable;
+		}
+
+		protected String getTargetTable() {
+			return targetTable;
+		}
+		
+		protected String getSourceJoinColumn() {
+			return sourceJoinColumn;
+		}
+
+		protected String getTargetJoinColumn() {
+			return targetJoinColumn;
+		}
+
+	}
 }
