97233bf842747637a31f11e780d2588ebe113e40
Zhihang Yao <zyao@virtualcitysystems.de>
2018-02-28 16:10:02 +0100

    use gradle as build tool

--- src/org/citydb/plugins/ade_manager/script/IDeleteScriptGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.citydb.plugins.ade_manager.script;
-
-import java.io.File;
-
-import org.citydb.database.DatabaseConnectionPool;
-
-public interface IDeleteScriptGenerator {
-	public void doProcess(DatabaseConnectionPool dbPool, File outputFile) throws DsgException;
-}
